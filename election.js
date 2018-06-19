5dc225972d7598aec985d4d9ef3ea001fbaa27d3
Karen <kjgamis@live.ca>
2018-06-19 17:48:58 -0400

    able to add form that will post vote

--- election.js
+++ election.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var list   = document.querySelector('#list');
   var index  = 0;
 
-  button.addEventListener('click', function() {
+  // button.addEventListener('click', function() {
 
     // console.log(event);
 
@@ -16,18 +16,31 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log(responseData);
 
       responseData.candidates.forEach( function (candidate) {
-        var liTag = document.createElement('li');
-        var h3Tag = document.createElement('h3');
-        var h4Tag = document.createElement('h4');
+        var form       = document.createElement('form')
+        form.method    = 'POST';
+        form.action    = 'https://bb-election-api.herokuapp.com/vote'
+        var hiddenId   = document.createElement('input');
+        hiddenId.type  = 'hidden';
+        hiddenId.name  = 'name'
+        hiddenId.value = responseData.candidates[index].name;
+        var submit     = document.createElement('input');
+        submit.type    = 'submit';
+        form.append(hiddenId);
+        form.append(submit);
+
+        var liTag      = document.createElement('li');
+        var h3Tag      = document.createElement('h3');
+        var h4Tag      = document.createElement('h4');
         h3Tag.innerText = "Name: " + responseData.candidates[index].name;
         h4Tag.innerText = "Votes: " + responseData.candidates[index].votes;
         liTag.append(h3Tag);
         liTag.append(h4Tag);
+        liTag.append(form);
         list.append(liTag);
         index++;
       });
     });
 
-  });
+  // });
 
 });
